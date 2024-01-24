9205977558743da8e2777335fe86fd841c7cc01f
Moiz-Ahmed236 <155655860+Moiz-Ahmed236@users.noreply.github.com>
2024-01-24 10:01:59 -0800

    Java Code

---
+++ java.js
@@ -0,0 +1,8 @@
+// console:
+let isStudent="I'm Intermediate Student by Pre-Engineering";
+let age="18";
+console.log(isStudent);
+console.log(age);
+
+// Alert:
+alert("Hello! Welcome to my web page; I'm MOIZ AHMED & I'm Also a Frontend Developer")
